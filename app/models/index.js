80cb82dc7354ee9623c9f3713c31289bc7562f73
Taggat4472 <tresor.mukendi@live.fr>
2023-03-15 10:28:08 +0100

    connexion à la db

---
+++ app/models/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+
+const {Sequelize, DataTypes} = require("sequelize");
+const sequelize = new Sequelize(
+  //  process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,
+ "restoapp","cda","azerty",
+  {
+  host: "127.0.0.1" ,
+  dialect: 'mariadb',
+  
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+// connexion mariaDB
+const db = require("./app/models");
+
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+  
