3f1860906102ce13913fd870cdeae5861275091a
Taggat4472 <tresor.mukendi@live.fr>
2023-03-15 12:00:53 +0100

    Activation api et port

--- server.js
+++ server.js
@@ -13,4 +13,13 @@ db.sequelize.sync()
     console.log("Failed to sync db: " + err.message);
   });
 
-  
+  // Test GET /
+app.get("/", (req, res) => {
+    res.json({ message: "Bonjour ! 👋." });
+  });
+ 
+  // Port
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
